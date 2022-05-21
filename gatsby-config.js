f6d3f9617a75ddb406528f3f8131068bdc16979f
Miskannn <maksim.kovtun2103@gmail.com>
2022-05-21 19:43:59 +0300

    build fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,12 +19,5 @@ module.exports = {
         path: `${__dirname}/src/pages/`,
       },
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images/`,
-      },
-    },
   ],
 }
